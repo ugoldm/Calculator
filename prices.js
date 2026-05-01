90a4308dd8de27d3b1fea310274d96af40ffdd3c
ugoldm <ugoldm03@gmail.com>
2026-05-01 20:00:17 +0300

    update prices

---
+++ prices.js
@@ -0,0 +1,18 @@
+window.PRICES = {
+  // Общие
+  'rail_2400':         116400,
+  'rail_3500':         169700,
+  'connector':          3900,
+  'end_clamp':          10200,
+  'mid_clamp':          9300,
+
+  // L-кронштейн
+  'l_bracket':          47800,
+
+  // Рег. угол
+  'front_stand':        38100,
+  'rear_stand':         96900,
+
+  // Мини-рельс
+  'mini_rail':          40600,
+};
