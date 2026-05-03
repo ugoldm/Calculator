d068baf025d9a9716a11e3701473a536f08d6f74
ugoldm <ugoldm03@gmail.com>
2026-05-03 11:50:55 +0300

    Revert "add auto currency convertion"
    This reverts commit 5d4edce5dfdd1f68d0f0c0762754b397583995be.

--- prices.js
+++ prices.js
@@ -1,18 +1,18 @@
 window.PRICES = {
   // Общие
-  'rail_2400': 116400,
-  'rail_3500': 169700,
-  'connector': 3900,
-  'end_clamp': 10200,
-  'mid_clamp': 9300,
+  'rail_2400':  { UZS: 116400, RUB: 730 },
+  'rail_3500':  { UZS: 169700, RUB: 1064 },
+  'connector':  { UZS: 3900, RUB: 24 },
+  'end_clamp':  { UZS: 10200, RUB: 64 },
+  'mid_clamp':  { UZS: 9300, RUB: 58 },
 
   // L-кронштейн
-  'l_bracket': 47800,
+  'l_bracket':  { UZS: 47800, RUB: 300 },
 
   // Рег. угол
-  'front_stand': 38100,
-  'rear_stand': 96900,
+  'front_stand': { UZS: 38100, RUB: 239 },
+  'rear_stand':  { UZS: 96900, RUB: 608 },
 
   // Мини-рельс
-  'mini_rail': 40600,
+  'mini_rail':  { UZS: 40600, RUB: 255 },
 };
